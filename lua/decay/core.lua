9cc2cac281b5817348806da7b9663121bb1b6cd5
Gabriel Guerra <gfranklings@gmail.com>
2022-06-28 12:28:58 -0400

    added light support

--- lua/decay/core.lua
+++ lua/decay/core.lua
@@ -58,12 +58,45 @@ local function get_decay()
   }
 end
 
+local function get_light_decay()
+  return {
+    background = "#dee1e6",
+    contrast = '#d9dce1',
+    statusline_bg = '#d4d7dc',
+    lighter = '#d4d7dc',
+    foreground = "#101419",
+    cursorline = '#cfd2d7',
+    comments = '#4d5768',
+    cursor = "#b6beca",
+    color0 = "#c5c8cd",
+    color1 = "#bd3c42",
+    color2 = "#69b373",
+    color3 = "#ceac67",
+    color4 = "#4d82c8",
+    color5 = "#a367cb",
+    color6 = "#519bc6",
+    color7 = "#101419",
+    color8 = "#989ba0",
+    color9 = "#c24147",
+    color10 = "#6eb878",
+    color11 = "#d3b16c",
+    color12 = "#5287cd",
+    color13 = "#a86cd0",
+    color14 = "#56a0cb",
+    color15 = "#1f2328",
+  }
+end
+
 function M.get_colors(dark)
-  dark = vim.g.decay_dark_mode or false
-  if dark then
-    return get_darker_decay()
+  if vim.o.background == 'dark' then
+    dark = vim.g.decay_dark_mode or false
+    if dark then
+      return get_darker_decay()
+    else
+      return get_decay()
+    end
   else
-    return get_decay()
+    return get_light_decay()
   end
 end
 
