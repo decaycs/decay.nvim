f36f1d92083c767c1789abd4eeca745109692117
Gabriel Guerra <gfranklings@gmail.com>
2023-02-18 12:14:00 -0400

    added the posibility to override palette

--- lua/decay/init.lua
+++ lua/decay/init.lua
@@ -1,32 +1,42 @@
+---@diagnostic disable: lowercase-global
 local M = {}
-local core = require('decay.core')
-local highlights = require('decay.highlights')
+local core = require("decay.core")
+local highlights = require("decay.highlights")
 
-function M.setup (opts)
-  if opts == nil then
-    opts = {
-      style = 'normal',
-      cmp = {
-        block_kind = false,
-      },
-      italics = {
-        code = true,
-        comments = false
-      }
-    }
-  end
-  -- disable bold
-  vim.cmd [[ set t_md= ]]
+vim = vim
+
+function M.setup(opts)
+	if opts == nil then
+		opts = {
+			style = "normal",
+			palette_overrides = {},
+			cmp = {
+				block_kind = false,
+			},
+			italics = {
+				code = true,
+				comments = false,
+			},
+		}
+	end
+	-- disable bold
+	vim.cmd([[ set t_md= ]])
 	vim.opt.termguicolors = true
-  vim.g.decay_style = opts.style
+	vim.g.decay_style = opts.style
 
-  local cmp_opts = opts.cmp or { block_kind = false }
+	local cmp_opts = opts.cmp or { block_kind = false }
 
-  vim.g.decay_cmp_block_kind = cmp_opts.block_kind == nil and true or cmp_opts.block_kind
+	vim.g.decay_cmp_block_kind = cmp_opts.block_kind == nil and true or cmp_opts.block_kind
 
-  local colors = core.get_colors(opts.style) -- getting the right palette
+	local colors = core.get_colors(opts.style) -- getting the right palette
 
-  highlights.highlight_all(colors, opts)
+	if opts.palette_overrides then
+		for key, color in pairs(opts.palette_overrides) do
+			colors[key] = color
+		end
+	end
+
+	highlights.highlight_all(colors, opts)
 end
 
 return M
