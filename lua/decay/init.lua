66abae397ad17169af7ed2003ef7e85963411c7c
musjj <72612857+musjj@users.noreply.github.com>
2023-05-23 01:33:33 +0700

    feat: respect the current user's configuration during `:colorscheme`
    This commit modularizes `setup` into `setup`, `load`, `set` and `get`.
    `setup` now stores the state using `set`, so that `colors/*.vim` can
    retrieve them. This also allows the user to call `set` to set the
    default options for each style ahead of time, without the overhead of
    loading each colorscheme over and over again.
    
    It's a bit hacky, but I wanted to keep the `setup` function
    backwards-compatible.

--- lua/decay/init.lua
+++ lua/decay/init.lua
@@ -5,10 +5,39 @@ local highlights = require("decay.highlights")
 
 vim = vim
 
-function M.setup(opts)
+M.state = {}
+
+function M.load(style, opts)
 	if opts == nil then
-		opts = {
-			style = "normal",
+		opts = M.state[style or "default"]
+	end
+
+	-- disable bold
+	vim.cmd([[ set t_md= ]])
+	vim.opt.termguicolors = true
+	vim.g.decay_style = style
+
+	local cmp_opts = opts.cmp or { block_kind = false }
+
+	vim.g.decay_cmp_block_kind = cmp_opts.block_kind == nil and true or cmp_opts.block_kind
+
+	local colors = core.get_colors(style) -- getting the right palette
+
+	if opts.palette_overrides then
+		for key, color in pairs(opts.palette_overrides) do
+			colors[key] = color
+		end
+	end
+
+	highlights.highlight_all(colors, opts)
+end
+
+function M.get(style)
+	vim.validate({
+		style = { style, "string" },
+	})
+	if type(M.state[style]) ~= "table" then
+		M.state[style] = {
 			palette_overrides = {},
 			cmp = {
 				block_kind = false,
@@ -19,24 +48,31 @@ function M.setup(opts)
 			},
 		}
 	end
-	-- disable bold
-	vim.cmd([[ set t_md= ]])
-	vim.opt.termguicolors = true
-	vim.g.decay_style = opts.style
+	return M.state[style]
+end
 
-	local cmp_opts = opts.cmp or { block_kind = false }
+function M.set(style, opts)
+	vim.validate({
+		style = { style, "string" },
+		opts = { opts, "table" },
+	})
+	M.state[style] = opts
+end
 
-	vim.g.decay_cmp_block_kind = cmp_opts.block_kind == nil and true or cmp_opts.block_kind
-
-	local colors = core.get_colors(opts.style) -- getting the right palette
-
-	if opts.palette_overrides then
-		for key, color in pairs(opts.palette_overrides) do
-			colors[key] = color
+function M.setup(opts)
+	local style = "default"
+	if type(opts) ~= "table" then
+		opts = M.get(style)
+	else
+		if opts.style == nil then
+			M.set(style, opts)
+		else
+			style = opts.style
+			opts.style = nil
+			M.set(style, opts)
 		end
 	end
-
-	highlights.highlight_all(colors, opts)
+	M.load(style, opts)
 end
 
 return M
